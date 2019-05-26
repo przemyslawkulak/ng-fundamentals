6f5ee4c03e7b058679d7a65a848fc2ca8c94fa35
unknown <przemyslaw.kulak86@gmail.com>
2019-05-26 21:03:51 +0200

    create-event component navigation on buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsDetailsComponent } from './events/events-details/events-details.component';
+import { CreateEventComponent } from './events/create-event/create-event.component';
 
 const routes: Routes = [
+  { path: 'events/new', component: CreateEventComponent },
   { path: 'events', component: EventsListComponent },
   { path: 'events/:id', component: EventsDetailsComponent },
+
   { path: '', redirectTo: '/events', pathMatch: 'full'}
 ];
 
