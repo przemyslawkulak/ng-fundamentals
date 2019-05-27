59d76734bb0be7daf943fdf37c536858e7f11943
unknown <przemyslaw.kulak86@gmail.com>
2019-05-27 11:45:18 +0200

    guard activate route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsDetailsComponent } from './events/events-details/events-details.component';
 import { CreateEventComponent } from './events/create-event/create-event.component';
+import { ErrorComponent } from './error/error.component';
+import { EventRouteActivatorService } from './events/events-details/event-route-activator.service';
 
 const routes: Routes = [
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events', component: EventsListComponent },
-  { path: 'events/:id', component: EventsDetailsComponent },
+  { path: 'events/:id', component: EventsDetailsComponent, canActivate: [EventRouteActivatorService] },
 
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: '404', component: ErrorComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,19 @@ import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 import { EventsDetailsComponent } from './events/events-details/events-details.component';
 import { CreateEventComponent } from './events/create-event/create-event.component';
+import { ErrorComponent } from './error/error.component';
+import { EventRouteActivatorService } from './events/events-details/event-route-activator.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent, EventsListComponent, EventsThumbnailComponent, NavComponent, EventsDetailsComponent, CreateEventComponent
+    AppComponent, EventsListComponent, EventsThumbnailComponent, NavComponent, EventsDetailsComponent, CreateEventComponent, ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [EventService, ToastrService],
+  providers: [EventService, ToastrService, EventRouteActivatorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
