e6d2ff41de8d93b221a8b784b0ce7ca2eb7a94be
unknown <przemyslaw.kulak86@gmail.com>
2019-05-28 14:58:31 +0200

    @inject decorator

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService} from './auth.service';
 import { Router } from '@angular/router';
+import { TOASTR_TOKEN, Toastr} from '../common/toastr.service'
 
 @Component({
   templateUrl: './profile.component.html',
@@ -20,7 +21,9 @@ export class ProfileComponent implements OnInit {
   private lastName: FormControl;
 
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService,
+    private router: Router,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr) {
 
   }
 
@@ -38,8 +41,8 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues) {
     if (this.profileForm.valid) {
     this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['events']);
-    }
+    this.toastr.success('Profile saved');
+  }
   }
 
   validateFirstName() {
